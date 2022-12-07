1049884ffc05beda59bf8722819cd180a458b15b
EDUARDO SILVA <educwb@gmail.com>
2022-12-07 12:22:38 -0300

    buscando um registro fixo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,50 @@
 import { useState} from 'react'
 import { db } from './firebaseConnection'
-import { doc, setDoc } from 'firebase/firestore'
+import { doc, setDoc, collection, addDoc, getDoc } from 'firebase/firestore'
 import './app.css'
+import { async } from '@firebase/util'
 
 export default function App(){
   const [titulo, setTitulo] = useState('')
   const [autor, setAutor] = useState('')
 
   async function handleAdd(){
-    await setDoc(doc(db, "posts", "12345"), {
+    // await setDoc(doc(db, "posts", "12345"), {
+    //   titulo: titulo,
+    //   autor: autor,
+    // })
+    // .then(() => {
+    //   console.log('Dados registrados no banco!')
+    // })
+    // .catch((error) => {
+    //   console.log('Gerou erro ' + error)
+    // })
+
+    await addDoc(collection(db, 'posts'), {
       titulo: titulo,
       autor: autor,
     })
     .then(() => {
-      console.log('Dados registrados no banco!')
+      console.log('Cadastrado com sucesso!')
+      setAutor('')
+      setTitulo('')
     })
     .catch((error) => {
-      console.log('Gerou erro ' + error)
+      console.log('Erro: ' + error)
+    })
+
+  }
+
+  async function buscarPost(){
+    const postRef = doc(db, 'posts', '12345')
+
+    await getDoc(postRef)
+    .then((snapshot) => {
+      setAutor(snapshot.data().autor)
+      setTitulo(snapshot.data().titulo)
+    })
+    .catch(() => {
+      console.log('Erro ao buscar!')
     })
   }
 
@@ -42,6 +70,7 @@ export default function App(){
         />
 
         <button onClick={handleAdd}>Cadastrar</button>
+        <button onClick={buscarPost}>Buscar</button>
 
       </div>
     </div>
