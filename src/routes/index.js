961af2f64599e9125d1b7dce6cdc5420d58f14dc
EDUARDO SILVA <educwb@gmail.com>
2022-12-22 12:59:57 -0300

    criando verificacao de acesso Private

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,8 @@ import Home from '../pages/Home'
 import Admin from '../pages/Admin'
 import Register from '../pages/Register'
 
+import Private from './Private'
+
 import Login from '../pages/Login'
 import Posts from '../pages/Posts'
 
@@ -11,8 +13,14 @@ function RoutesApp(){
   return(
     <Routes>
       <Route path='/' element={ <Home/> } />
-      <Route path='/admin' element={ <Admin/> } />
       <Route path='/register' element={ <Register/> } />
+      
+      <Route path='/admin' element={ 
+        <Private>
+          <Admin/>
+        </Private>
+      }/>
+
       <Route path='/login' element={ <Login/> } />
       <Route path='/posts' element={ <Posts/> } />
     </Routes>
