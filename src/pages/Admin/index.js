e1d8df4f51a16b7fac9b5c3097aa7186923252d0
EDUARDO SILVA <educwb@gmail.com>
2022-12-25 07:59:11 -0300

    problemas com dependencias

--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -1,9 +1,20 @@
-import './admin.css'
+import './styleHome.scss'
+import ReactTypingEffect from 'react-typing-effect'
 
-export default function Admin(){
-  return(
-    <div>
-      <h1>Página ADMIN</h1>
-    </div>
-  )
+export const Admin = () => {
+    return(
+        <div className="containerHome">
+          <h1>
+            <ReactTypingEffect 
+              speed={'200'} 
+              eraseSpeed={'100'}
+              eraseDelay={'400'}
+              typingDelay={'300'}
+              text={["R.L.GOUVEA","ADMINISTRAÇÃO DE BENS", "COMPRA - VENDA - LOCAÇÕES"]} 
+            /> 
+          </h1>
+        </div>
+    )
 }
+
+export default Admin
